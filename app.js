bda897ac534e902dbb6141ad955f1a480e31016c
bpuuqd <bpuuqd@gmail.com>
2021-03-30 01:20:48 +0900

    hasing with salting

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const port = 3000;
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static('public'));
@@ -34,9 +35,12 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
+
+   bcrypt.hash(req.body.password,saltRounds,function(err, hash) {
+
   const newUser = new User({
     email : req.body.username,
-    password: md5(req.body.password)
+    password: hash
   });
   newUser.save(function(err) {
     if(err){
@@ -45,11 +49,12 @@ app.post("/register", function(req, res) {
       res.render("secrets");
     }
   });
+  });
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({
     email : username
@@ -58,15 +63,16 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true){
+
+            res.render("secrets");
+          }
+        });
         }
       }
-    }
-
-  })
-
-})
+      });
+});
 
 
 
