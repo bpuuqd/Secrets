c38e48d6424c3b240541d30890e6872dbd2b2c6d
bpuuqd <bpuuqd@gmail.com>
2021-03-30 01:00:05 +0900

    hasing

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const port = 3000;
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 const app = express();
-
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,9 +18,6 @@ const userSchema = new mongoose.Schema({
   email : String,
   password: String
 });
-
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields : ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,7 +36,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email : req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if(err){
@@ -54,7 +49,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({
     email : username
